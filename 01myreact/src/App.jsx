d8a17e8ab260f6416624cf20a14cd348f8f97eff
sawan patil <sawanpatil1711@gmail.com>
2026-01-05 16:03:29 +0530

    fix: update App component to display FirstComponent and correct greeting

--- 01myreact/src/App.jsx
+++ 01myreact/src/App.jsx
@@ -3,8 +3,8 @@ import FirstComponent from "./first";
 function App() {
   return (
     <div>
-      <FirstComponent />
-      <h1>Hello, world!</h1>
+      <h2>First Component</h2>
+      <h1>hellow sawan</h1>
     </div>
   );
 }
